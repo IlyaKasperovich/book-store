ec2e254a2a3c0aa8e0efbf96cc06f5d81c83ca3e
Ilya Kasperovich <kasper.ii2108@gmail.com>
2020-05-05 21:38:55 +0300

    Updates

---
+++ precache-manifest.b5b8ed0fcd2fdc8ce41322bbcbd0f954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853b6dce9a986105963dbdc4e910bd4e",
+    "url": "/book-store/index.html"
+  },
+  {
+    "revision": "bc6c61601a80385c5201",
+    "url": "/book-store/static/css/main.81cf24b7.chunk.css"
+  },
+  {
+    "revision": "7dec4046519407bf0db1",
+    "url": "/book-store/static/js/2.47f1a0cc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/book-store/static/js/2.47f1a0cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc6c61601a80385c5201",
+    "url": "/book-store/static/js/main.4ff15a3e.chunk.js"
+  },
+  {
+    "revision": "1fedc942ab78878a130f",
+    "url": "/book-store/static/js/runtime-main.985857a3.js"
+  }
+]);
